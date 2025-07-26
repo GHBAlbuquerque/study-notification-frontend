4010c1a13e51fe1a512ffdfc2649b3456ed71ce0
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-07-26 14:17:06 -0300

    feat: main form adjustments and table config in lists

--- src/pages/ListNotifications/index.tsx
+++ src/pages/ListNotifications/index.tsx
@@ -1,15 +1,24 @@
 import { useEffect } from "react";
 import { Container } from "../../components/Container";
 import { Heading } from "../../components/Heading";
+import styles from "./styles.module.css";
 
 export function ListNotifications() {
   useEffect(() => {
-    document.title = 'Gila - List Notifications';
+    document.title = "Gila - List Notifications";
   }, []);
 
   return (
-    <Container>
-      <Heading>List Notifications</Heading>
-    </Container>
+    <>
+      <Container>
+        <Heading>List Notifications</Heading>
+      </Container>
+
+      <Container>
+        <div className={styles.notificationsTable}>
+
+        </div>
+      </Container>
+    </>
   );
 }
