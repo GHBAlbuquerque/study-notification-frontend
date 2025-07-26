52bdead2189b5358c93ac3f32b01cdb36d9aa7e5
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-07-26 14:10:11 -0300

    feat: document title adjustments and svg icon

--- src/pages/CreateNotification/index.tsx
+++ src/pages/CreateNotification/index.tsx
@@ -1,8 +1,13 @@
 import { Container } from "../../components/Container";
 import MainForm from "../../components/MainForm";
 import { Heading } from "../../components/Heading";
+import { useEffect } from "react";
 
 export function CreateNotification() {
+  useEffect(() => {
+    document.title = 'Gila - Create Notification';
+  }, []);
+
   return (
     <>
       <Container>
--- src/pages/ListNotifications/index.tsx
+++ src/pages/ListNotifications/index.tsx
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
 import { Container } from "../../components/Container";
 import { Heading } from "../../components/Heading";
 
 export function ListNotifications() {
+  useEffect(() => {
+    document.title = 'Gila - List Notifications';
+  }, []);
+
   return (
     <Container>
       <Heading>List Notifications</Heading>
